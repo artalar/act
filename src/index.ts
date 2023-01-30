8c8ec1d5a0d59ec75100a90fd9c73dea34666f42
artalar <artalarut@proton.me>
2023-01-30 04:28:49 +0000

    improve codestyle

--- src/index.ts
+++ src/index.ts
@@ -1,140 +1,166 @@
+// #region TYPES
+
 interface Subscriber {
   (): void
   _v: Array<ActValue>
 }
 
-interface Subscribable<T = any> {
-  subscribe(cb: (state: T) => any): () => void
-}
-
-export interface ActValue<T = any> extends Subscribable<T> {
-  (state?: T): T
+/** Base mutable reactive primitive, a leaf of reactive graph */
+export interface ActValue<T = unknown> {
+  (newState?: T): T
+  subscribe(cb: (state: T) => void): () => void
   _s: Set<Subscriber>
 }
-export interface ActComputed<T = any> extends Subscribable<T> {
+export interface ActComputed<T = unknown> {
   (): T
+  subscribe(cb: (state: T) => void): () => void
 }
 
-// a subscriber - source of truth
-let root: null | Subscriber = null
-// list of a publishers from a computed in prev stack step
-// we store a structure here (i=dep, i+1=depState)
-let pubs: null | Array<any> = null
-// global `dirty` flag used to cache visited nodes during it invalidation by a subscriber
-let version = 0
-// subscribers queue for a batch, also used as a cache key of a transaction
-let queue: Array<Set<() => any>> = []
+export type Act<T = unknown> = ActValue<T> | ActComputed<T>
 
-// @ts-expect-error
+/** List of used dependencies to memorize computed function */
+interface Dependencies
+  extends Array<{
+    /** Pulled dependency */
+    act: Act
+    /** The dependency returned state */
+    state: unknown
+  }> {}
+
+// #endregion
+
+/** subscribers from all touched signals */
+let QUEUE: Array<Set<Subscriber>> = []
+
+/** global queue cache flag */
+let QUEUE_VERSION = 0
+
+/** current subscriber during invalidation */
+let SUBSCRIBER: null | Subscriber = null
+
+/** global subscriber pull cache flag */
+let SUBSCRIBER_VERSION = 0
+
+/** stack-based parent ref to silently link nodes */
+let DEPS: null | Dependencies = null
+
+// @ts-expect-error `let` is more performant, but broke the types
 export let act: {
   <T>(computed: () => T, equal?: (prev: T, next: T) => boolean): ActComputed<T>
-  <T>(state: T): ActValue<T>
-
+  <T>(initState: T): ActValue<T>
   notify: () => void
-} = (s, equal?: (prev: any, next: any) => boolean): any => {
-  let _version = -1
-  let a: ActValue<any> & ActComputed<any>
+} = (init, equal) => {
+  let version = -1
+  let state: any
+  let theAct: ActValue<any> & ActComputed<any>
 
-  if (typeof s === 'function') {
-    let _pubs: Array<any> = []
-    let computed = s as () => any
-    // @ts-expect-error
-    a = () => {
-      if (_version !== version || root === null) {
-        let prevPubs = pubs
-        pubs = null
+  if (typeof init === 'function') {
+    let deps: Dependencies = []
+    // @ts-expect-error expected properties declared below
+    theAct = () => {
+      if (version !== SUBSCRIBER_VERSION) {
+        version = SUBSCRIBER_VERSION
 
-        let isActual = _pubs.length > 0
-        for (let i = 0; isActual && i < _pubs.length; i += 2) {
-          isActual = _pubs[i]() === _pubs[i + 1]
-        }
-
-        if (!isActual) {
-          pubs = _pubs = []
-          let newState = computed()
-          if (_version === -1 || equal === undefined || !equal(s, newState)) {
-            s = newState
-          }
-        }
-
-        pubs = prevPubs
-
-        _version = version
-      }
-
-      pubs?.push(a, s)
-
-      return s
-    }
-  } else {
-    // @ts-expect-error
-    a = (newState) => {
-      if (newState !== undefined && newState !== s) {
-        s = newState
-
-        if (queue.push(a._s) === 1) Promise.resolve().then(act.notify)
-
-        a._s = new Set()
-      }
-
-      pubs?.push(a, s)
-
-      if (_version !== version) {
-        _version = version
-        if (root) {
-          a._s.add(root)
-          root._v.push(a)
-        }
-      }
-
-      return s
-    }
-    a._s = new Set()
-  }
-
-  a.subscribe = (cb) => {
-    let lastQueue: any = cb
-    let lastState: any = cb
-    // @ts-expect-error
-    let subscriber: Subscriber = () => {
-      if (lastQueue !== queue) {
-        lastQueue = queue
-
-        ++version
-
-        let prevRoot = root
-        root = subscriber
-
-        un()
-        subscriber._v = []
+        let prevDeps = DEPS
+        DEPS = null
 
         try {
-          if (lastState !== a()) cb((lastState = s))
+          if (deps.length === 0 || deps.some((el) => el.state !== el.act())) {
+            DEPS = deps = []
+
+            let newState = init()
+
+            if (
+              equal === undefined ||
+              // first call
+              state === undefined ||
+              !equal(state, newState)
+            ) {
+              state = newState
+            }
+          }
         } finally {
-          root = prevRoot
+          DEPS = prevDeps
+        }
+      }
+
+      DEPS?.push({ act: theAct, state })
+
+      return state
+    }
+  } else {
+    state = init
+    // @ts-expect-error expected properties declared below
+    theAct = (newState) => {
+      if (newState !== undefined && newState !== state) {
+        // mark all computeds dirty
+        ++SUBSCRIBER_VERSION
+
+        state = newState
+
+        if (QUEUE.push(theAct._s) === 1)
+          Promise.resolve().then(act.notify)
+
+        theAct._s = new Set()
+      }
+
+      if (version !== SUBSCRIBER_VERSION && SUBSCRIBER !== null) {
+        version = SUBSCRIBER_VERSION
+        theAct._s.add(SUBSCRIBER)
+        SUBSCRIBER._v.push(theAct)
+      }
+
+      DEPS?.push({ act: theAct, state })
+
+      return state
+    }
+    theAct._s = new Set()
+  }
+
+  theAct.subscribe = (cb) => {
+    let queueVersion = -1
+    let lastState: unknown = Symbol()
+
+    // @ts-expect-error `let` could be more performant than `const`
+    let subscriber: Subscriber = () => {
+      if (queueVersion !== QUEUE_VERSION) {
+        try {
+          queueVersion = QUEUE_VERSION
+
+          un()
+          subscriber._v = []
+
+          SUBSCRIBER = subscriber
+
+          SUBSCRIBER_VERSION++
+
+          if (theAct() !== lastState) cb((lastState = state))
+        } finally {
+          SUBSCRIBER = null
         }
       }
     }
     subscriber._v = []
 
-    const un = () => {
-      for (const val of subscriber._v) {
-        val._s.delete(subscriber)
+    let un = () => {
+      for (let signal of subscriber._v) {
+        signal._s.delete(subscriber)
       }
     }
 
     subscriber()
 
-    // TODO next tick?
     return un
   }
 
-  return a
+  return theAct
 }
 act.notify = () => {
-  const iterator = queue
+  QUEUE_VERSION++
 
-  queue = []
+  let iterator = QUEUE
+
+  QUEUE = []
 
   for (let subscribers of iterator) {
     for (let subscriber of subscribers) subscriber()
